46fed576cffb486993d2dbb3cf444dd365fd6823
Ajulrag <ajulrag353@gmail.com>
2024-01-16 13:15:05 +0530

    feat: Database connection

---
+++ src/database.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const dbUrl = 'mongodb://127.0.0.1:27017/mongocrud';
+
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
---
+++ src/ui/app.js
@@ -0,0 +1,9 @@
+const taskForm = document.querySelector('#taskForm');
+const taskName = document.querySelector('#taskName');
+const taskDescription = document.querySelector('#taskDescription');
+
+taskForm.addEventListener('submit', e => {
+    e.preventDefault();
+    console.log(taskName.value,taskDescription.value);
+    taskForm.reset();
+})
